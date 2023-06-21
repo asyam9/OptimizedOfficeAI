52fd4f86a75db7b9734faabafcef975dc98ae37c
DaonWoori <alwjd7858@gmail.com>
2023-06-21 11:23:12 +0900

    리뷰 작성 페이지 1차 구현(디자인 적용 x)

--- Review_site/Reviews/views.py
+++ Review_site/Reviews/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse
 from django.views.generic import ListView
 from Reviews.models import Review_Models
+from .forms import FileUploadForm
 
 # Create your views here.
 class IndexView(ListView):
@@ -13,8 +14,40 @@ class IndexView(ListView):
 
     def index(self, request):
         reviews = self.model.objects.all()
-        return render(request, "Reviews/index.html", {'reviews':reviews})
+        return render(request, self.template_name, {'reviews':reviews})
 
+# 리뷰 작성 페이지 로직
 def upload(request):
-    """리뷰 작성 페이지"""
-    return HttpResponse("리뷰작성페이지")
+    ### 사용자가 폼을 통해 입력을 했을 경우, 
+    if request.method == 'POST':
+
+        # 입력값 받아오기
+        title = request.POST['title']
+        ratings = request.POST['ratings']
+        content = request.POST['content']
+        img = request.FILES["imgfile"]
+
+        # 새로운 데이터 생성
+        new_file = Review_Models(
+            title = title,
+            ratings = ratings,
+            content = content,
+            imgfile = img
+        )
+
+        # 입력받은 데이터를 데이터베이스에 저장 -> 자동으로 id 생성
+        new_file.save()
+
+        # 결과보기 페이지에 id 값 같이 넘겨주기
+        return redirect('detail', post_id=new_file.id)
+    
+    ### 사용자가 입력하기 전, 폼을 화면에 출력해주는 것
+    else:
+        fileuploadForm = FileUploadForm()
+        context = {
+            'fileuploadForm': fileuploadForm,
+        }
+        return render(request, 'Reviews/review_upload.html', context)
+    
+def detail(request, post_id):
+    return HttpResponse("상세보기 페이지" + str(post_id))
