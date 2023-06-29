bad4e3411c9c2772379ddb1a85c419fe3c91612e
DaonWoori <alwjd7858@gmail.com>
2023-06-29 10:31:12 +0900

    테스트 용 웹 폴더 삭제

--- dev_django/image_pro/fileupload/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import FileUpload
-
-# 사용자의 데이터를 입력받을 수 있는 양식 ㅡ> 모델 폼 사용
-class FileUploadForm(ModelForm):
-    class Meta:
-        model = FileUpload # 사용할 모델 불러오기
-        fields = ['imgfile'] # 모델 필드 중 사용자의 입력이 필요한 필드 지정
--- dev_django/image_pro/fileupload/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import fileUpload, result
-
-urlpatterns = [
-	path('fileupload/', fileUpload, name="fileupload"),
-    path('fileupload/result/<int:post_id>', result, name='result')
-    # path('result/', result, name='result')
-]
